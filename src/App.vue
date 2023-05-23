a58bf0bda4e4940fd5f1270fb0ef4cd951d796f4
JohnGu6 <cscode1024@163.com>
2023-05-23 22:23:27 +0800

    Page foundation construction

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="app">
     <RouterView></RouterView>
-    <RouterLink to="/home">首页</RouterLink>
-    <RouterLink to="/collect">收藏</RouterLink>
-    <RouterLink to="/order">订单</RouterLink>
-    <RouterLink to="/message">消息</RouterLink>
+    <Tabbar></Tabbar>
   </div>
 </template>
 
 <script setup>
-
+import Tabbar from './components/TabBar/tabbar.vue';
 </script>
 <style scoped>
 </style>
---
+++ src/components/TabBar/tabbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tabbar">
+	<div class="tabbar-item">
+		<img src="@/assets/img/tabbar/tab_home.png" alt="">
+		<span class="text">首页</span>
+	</div>
+	<div class="tabbar-item">
+		<img src="@/assets/img/tabbar/tab_collect.png" alt="">
+		<span class="text">收藏</span>
+	</div>
+	<div class="tabbar-item">
+		<img src="@/assets/img/tabbar/tab_order.png" alt="">
+		<span class="text">订单</span>
+	</div>
+	<div class="tabbar-item">
+		<img src="@/assets/img/tabbar/tab_message.png" alt="">
+		<span class="text">消息</span>
+	</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+<style lang="less" scoped>
+
+.tabbar {
+	display: flex;
+	height: 50px;
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	border-top: 1px solid #f3f3f3;
+
+	.tabbar-item {
+		flex: 1;
+		display: flex;
+		flex-direction: column; 
+		justify-content: center;
+		align-items: center;
+		
+		.text {
+			font-size: 12px;
+			margin-top: 2px;
+		}
+		img {
+			width: 32px;
+		}
+	}
+}
+</style>
