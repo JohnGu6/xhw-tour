2a281571efac8d6c9ffa92c38f63b42fd37b6f10
JohnGu6 <cscode1024@163.com>
2023-05-27 14:25:00 +0800

    update tabbar vant

--- src/components/TabBar/tabbar copy.vue
+++ src/components/TabBar/tabbar copy.vue
@@ -1,60 +1,62 @@
 <template>
-  <div class="tabbar">
-	<template v-for="(item, index) in tabbarData">
-		<div class="tabbar-item" 
-			:class="{ active:currentIndex === index }"
-			@click="itemClick(index)"
-			>
-			<img v-if ="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
-			<img v-else :src="getAssetURL(item.imageActive)" alt="">
-			<span class="text">{{ item.text}}</span>
-		</div>
-	</template>
-  </div>
-</template>
-
-<script setup>
-
-import tabbarData from "@/assets/data/tabbar.js"
-import { getAssetURL } from "@/utils/get-asset-imgurl";
-import { ref } from "vue";
-
-const currentIndex = ref(0)
-
-const itemClick = (index) => {
-	currentIndex.value = index
-}
-
-</script>
-<style lang="less" scoped>
-
-.tabbar {
-	display: flex;
-	height: 50px;
-	position: fixed;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	border-top: 1px solid #f3f3f3;
-
-	.tabbar-item {
-		flex: 1;
-		display: flex;
-		flex-direction: column; 
-		justify-content: center;
-		align-items: center;
-
-		&.active {
-			color: var(--primary-color);
-		}
-		
-		.text {
-			font-size: 12px;
-			margin-top: 2px;
-		}
-		img {
-			width: 32px;
-		}
-	}
-}
-</style>
+	<div class="tabbar">
+	  <template v-for="(item, index) in tabbarData">
+		  <div class="tabbar-item" 
+			  :class="{ active:currentIndex === index }"
+			  @click="itemClick(index, item)"
+			  >
+			  <img v-if ="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
+			  <img v-else :src="getAssetURL(item.imageActive)" alt="">
+			  <span class="text">{{ item.text}}</span>
+		  </div>
+	  </template>
+	</div>
+  </template>
+  
+  <script setup>
+  
+  import tabbarData from "@/assets/data/tabbar.js"
+  import { getAssetURL } from "@/utils/get-asset-imgurl";
+  import { ref } from "vue";
+  import { useRouter } from "vue-router";
+  
+  const currentIndex = ref(0)
+  const router = useRouter()
+  const itemClick = (index, item) => {
+	  currentIndex.value = index
+	  router.push(item.path)
+  }
+  
+  </script>
+  <style lang="less" scoped>
+  
+  .tabbar {
+	  display: flex;
+	  height: 50px;
+	  position: fixed;
+	  bottom: 0;
+	  left: 0;
+	  right: 0;
+	  border-top: 1px solid #f3f3f3;
+  
+	  .tabbar-item {
+		  flex: 1;
+		  display: flex;
+		  flex-direction: column; 
+		  justify-content: center;
+		  align-items: center;
+  
+		  &.active {
+			  color: var(--primary-color);
+		  }
+		  
+		  .text {
+			  font-size: 12px;
+			  margin-top: 2px;
+		  }
+		  img {
+			  width: 32px;
+		  }
+	  }
+  }
+  </style>
--- src/components/TabBar/tabbar.vue
+++ src/components/TabBar/tabbar.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="tabbar">
-	<template v-for="(item, index) in tabbarData">
-		<div class="tabbar-item" 
-			:class="{ active:currentIndex === index }"
-			@click="itemClick(index, item)"
-			>
-			<img v-if ="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
-			<img v-else :src="getAssetURL(item.imageActive)" alt="">
-			<span class="text">{{ item.text}}</span>
-		</div>
-	</template>
+	<van-tabbar v-model="currentIndex" active-color="#ff9854">
+		<template v-for="(item, index) in tabbarData">
+			<van-tabbar-item :to="item.path">
+			<span>{{item.text}}</span>
+    		<template #icon>
+				<img v-if ="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
+			  	<img v-else :src="getAssetURL(item.imageActive)" alt="">	
+    		</template>
+		</van-tabbar-item>
+	  </template>
+</van-tabbar>
+
   </div>
 </template>
 
@@ -18,45 +20,16 @@
 import tabbarData from "@/assets/data/tabbar.js"
 import { getAssetURL } from "@/utils/get-asset-imgurl";
 import { ref } from "vue";
-import { useRouter } from "vue-router";
 
 const currentIndex = ref(0)
-const router = useRouter()
-const itemClick = (index, item) => {
-	currentIndex.value = index
-	router.push(item.path)
-}
 
 </script>
 <style lang="less" scoped>
 
 .tabbar {
-	display: flex;
-	height: 50px;
-	position: fixed;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	border-top: 1px solid #f3f3f3;
-
-	.tabbar-item {
-		flex: 1;
-		display: flex;
-		flex-direction: column; 
-		justify-content: center;
-		align-items: center;
-
-		&.active {
-			color: var(--primary-color);
-		}
-		
-		.text {
-			font-size: 12px;
-			margin-top: 2px;
-		}
-		img {
-			width: 32px;
-		}
+	--van-tabbar-item-icon-size:30px !important;
+	img{
+		height: 28px;
 	}
 }
 </style>
